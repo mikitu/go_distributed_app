0634e4bb7938ad908b4e948a61238ef2c5421ad9
Mihai Bucse <mbucse@mediamath.com>
2016-05-18 16:34:55 +0100

    publish to queue

---
+++ src/distributed/qutils/qutils.go
@@ -0,0 +1,35 @@
+package qutils
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+const SensorlistQueue = "SensorList"
+
+func GetChannel(url string) (*amqp.Connection, *amqp.Channel) {
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to estabilish connection to message broker")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to get channel for connection")
+	return conn, ch
+}
+func GetQueue(name string, ch *amqp.Channel) *amqp.Queue {
+	q, err := ch.QueueDeclare(
+		name,  // name string
+		false, //durable bool
+		false, //autoDelete boolean
+		false, //exclusive boolean,
+		false, //noWait boolean,
+		nil)   //args amqp.Table)
+	failOnError(err, "Failed to declare queue")
+	return &q
+}
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatal("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
